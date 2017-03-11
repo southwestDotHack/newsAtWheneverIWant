911fe2743a8dc681b69ca66fb0883d5e4ba51e9b
Marry Child <Marry Child>
2017-03-11 03:55:22 -0700

    We have a basic hello world! (note that this directory will NOT hold the final project

---
+++ GoalOne/app/src/main/java/firsttrys/com/goalone/MainActivity.java
@@ -0,0 +1,13 @@
+package firsttrys.com.goalone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
